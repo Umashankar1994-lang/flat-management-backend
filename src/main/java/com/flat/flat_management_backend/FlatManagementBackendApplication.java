de028e65e962fefb1f35bdfffc5a77995a46e11c
Umashankar Kundu <umashankar.kundu1@ibm.com>
2026-02-13 04:22:36 +0530

    First structure is created

---
+++ src/main/java/com/flat/flat_management_backend/FlatManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.flat.flat_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatManagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flat/flat_management_backend/FlatManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flat.flat_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
